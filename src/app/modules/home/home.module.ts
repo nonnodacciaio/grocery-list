51c33cb2d5aec2c64a79c99f1fc18e35edf31ba3
nonnodacciaio <nonnodacciaio@gmail.com>
2023-10-03 11:04:42 +0200

    Defined db structure

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from "@angular/core";
-
 import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
 import { HomePage } from "./home.page";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { Router, RouterModule, Routes } from "@angular/router";
+import { MatButtonModule } from "@angular/material/button";
+
+const routes: Routes = [{ path: "", component: HomePage }];
 
 @NgModule({
 	declarations: [HomePage],
-	imports: [CommonModule]
+	imports: [CommonModule, RouterModule.forChild(routes), MatFormFieldModule, MatInputModule, MatIconModule, MatAutocompleteModule, MatButtonModule]
 })
 export class HomeModule {}
--- src/app/modules/home/home.page.ts
+++ src/app/modules/home/home.page.ts
@@ -1,4 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { Firestore } from "@angular/fire/firestore";
 
-@Component({ selector: "home", template: `` })
-export class HomePage {}
+@Component({
+	selector: "home",
+	template: `
+		<mat-form-field class="w-full">
+			<mat-label>Add or search item</mat-label>
+			<input
+				matInput
+				type="text" />
+			<button
+				matSuffix
+				mat-icon-button
+				aria-label="Add item"
+				color="primary">
+				<mat-icon>add</mat-icon>
+			</button>
+		</mat-form-field>
+	`
+})
+export class HomePage {
+	private firestore: Firestore = inject(Firestore);
+}
