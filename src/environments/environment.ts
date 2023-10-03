37544169ad2e92e7f1de5b3a6247ade7890326f5
nonnodacciaio <nonnodacciaio@gmail.com>
2023-10-03 10:19:41 +0200

    Installed firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'grocery-list-57c0c',
+    appId: '1:292156208605:web:8a3daecd40ff96a85b7234',
+    storageBucket: 'grocery-list-57c0c.appspot.com',
+    apiKey: 'AIzaSyA07xblvjVYSbuTdzTnfoM7cWDSggu9jLI',
+    authDomain: 'grocery-list-57c0c.firebaseapp.com',
+    messagingSenderId: '292156208605',
+    measurementId: 'G-JMS0N7GDCF',
+  },
+};
